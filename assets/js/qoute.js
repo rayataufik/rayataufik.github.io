70f83e104d9b66d3a50fc800479a1ef3a109a45e
rayataufik <rayataufik4@gmail.com>
2021-10-08 22:20:04 +0700

    Firts Commit

--- assets/js/qoute.js
+++ assets/js/qoute.js
@@ -1,6 +1,6 @@
 const Quote = document.getElementById("quote");
 const Author = document.getElementById("author");
-fetch("http://quotes.stormconsultancy.co.uk/random.json")
+fetch("https://quotes.stormconsultancy.co.uk/random.json")
   .then((res) => {
     return res.json();
   })
